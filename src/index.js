1102ab4eb261b56146408c746aa890859c6a5bff
Paul Petrov <paul@bargamut.ru>
2020-10-28 14:33:49 +0300

    Install Babel & configure Webpack & Babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log(`Start`);
+class Game {
+	constructor() {
+		this.name = `Violin Charades`;
+	}
+}
+
+const myGame = new Game();
+
+console.log(`I like ${myGame.name}.`);
