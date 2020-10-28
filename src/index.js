c147925a0ad2286a78ce4be0c58b8e1271e5f8d3
Paul Petrov <paul@bargamut.ru>
2020-10-28 14:50:15 +0300

    Configure Webpack for import images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+import kitty from './img/kitty.jpg';
+
 class Game {
 	name = `Violin Charades`;
+	img = kitty;
 }
 
 const myGame = new Game();
 
-console.log(`I like ${myGame.name}.`);
+console.log(`I like ${myGame.name} and ${myGame.img}.`);
