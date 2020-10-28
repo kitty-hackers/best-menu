cbf7c100f2e594b3afa9dbaaab3f873e279acf01
Paul Petrov <paul@bargamut.ru>
2020-10-28 17:30:43 +0300

    Start split Webpack config to dev & prod

---
+++ config/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		main: path.resolve(__dirname, `./src/index.js`),
+	},
+	output: {
+		path: path.resolve(__dirname, `./dist`),
+		filename: `[name].bundle.js`,
+	},
+	mode: `development`,
+	devServer: {
+		historyApiFallback: true,
+		contentBase: path.resolve(__dirname, `./dist`),
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8080,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: `webpack Boilerplate`,
+			template: path.resolve(__dirname, `./src/template.html`),
+			filename: `index.html`,
+		}),
+		new CleanWebpackPlugin(),
+		// apply changes in case of hot reloading only
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [ `babel-loader` ],
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: `asset/resource`,
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+				type: `asset/inline`,
+			},
+			{
+				test: /\.css$/,
+				// style-loader - для dev, для prod - MiniCssExtractPlugin
+				use: [ `style-loader`, `css-loader`, `postcss-loader` ],
+			},
+		]
+	},
+};
