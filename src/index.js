eb9c3b5451ebdbced87d4ed52bd679f70c07d726
Paul Petrov <paul@bargamut.ru>
2020-10-28 14:59:08 +0300

    Configure Webpack for import fonts & inline data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 'use strict';
 
 import kitty from './img/kitty.jpg';
+import woman from './img/wow_woman.svg';
+import font from './fonts/SPLENDOR_rus.ttf';
 
 class Game {
 	name = `Violin Charades`;
 	img = kitty;
+	vector = woman;
+	font = font;
 }
 
 const myGame = new Game();
 
 console.log(`I like ${myGame.name} and ${myGame.img}.`);
+console.log(`Also ${myGame.vector}.`);
+console.log(`Also ${myGame.font}.`);
