987c00f14fffed6da0d404440ddc14a560b7b8e6
Paul Petrov <paul@bargamut.ru>
2020-10-30 18:49:27 +0300

    Install React test renderer & create App snapshot

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,10 @@
 'use strict';
 
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import App from './App';
+
 const mock = {
   test: true,
 };
@@ -8,4 +13,12 @@ it(`should be truely`, () => {
   const { test } = mock;
 
   expect(test).toBe(true);
+});
+
+it(`should render App snapshot`, () => {
+  const app = renderer
+    .create(<App />)
+    .toJSON();
+
+  expect(app).toMatchSnapshot();
 });
